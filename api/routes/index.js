d9b3cc63458413b3affdb362cf8310f92c7f477a
AshileySabah <ashileysabrina2002@gmail.com>
2022-09-03 15:07:59 -0300

    feat: crud nivel

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,9 @@
 const bodyParser = require('body-parser')
 const pessoas = require('./pessoasRoute.js')
+const niveis = require('./niveisRoute.js')
 
 module.exports = app => {
     app.use(bodyParser.json())
     app.use(pessoas)
+    app.use(niveis)
 }
